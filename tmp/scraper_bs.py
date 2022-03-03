49f794b2d68271ed39475b3179250564bf1ba444
Sebastian Smyk <stalminator@gmail.com>
2022-03-03 22:49:15 +0100

    temp

--- tmp/scraper_bs.py
+++ tmp/scraper_bs.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-page_url = 'https://www.amazon.pl/dp/B085K45C3S'
-page = requests.get(page_url, cookies={'__hs_opt_out': 'no'})
+page_url = 'https://www.amazon.pl/dp/B09BL2DPWV/'
+page = requests.get(page_url, timeout=None)
 soup = BeautifulSoup(page.content, 'html.parser')
 product_name = soup.title.string.split(':')[0]
 price = float(soup.find(id='tp_price_block_total_price_ww').get_text().split('zł')[0].replace('\xa0','').replace(',','.'))
@@ -17,4 +17,6 @@ image = soup.find(id="landingImage")['data-old-hires']
 print(product_name)
 print(price)
 print(image)
+
+
 #print(image.get('data-old-hires'))
